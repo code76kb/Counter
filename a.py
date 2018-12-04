3df04dd3884d33be9e126821559bcf3f10c046fd
Rahul Patidar <code76kb@gmail.com>
2018-12-04 20:58:34 +0530

    done

--- a.py
+++ a.py
@@ -30,6 +30,10 @@ def init():
 def sigmoid(x):
     return (1/(1+np.exp(-x)))
 
+#Sigmoid prime
+def sigmoidPrime(x):
+    return (np.exp(-x)/ ((1+np.exp(-x))**2) )
+
 # Step Activation
 def step(x):
     return (np.where(x > 0.5,1,0))
@@ -45,12 +49,14 @@ def d2b(decimal):
 
 # Run
 def run():
+    global weight_matrix_1,weight_matrix_2,weight_matrix_output
     init()
     input = np.array(list(d2b(0)),dtype='uint8')
     inputDec = 0
     count = 0
-    while(count < 1):
-        inputDec = b2d(np.array2string(input,separator='')[1:-1])
+    while(count < 10000):
+        arraystring = np.array2string(input,separator='')[1:-1]
+        inputDec = b2d(arraystring)
         target = np.array(list(d2b(inputDec+1)),dtype='uint8')
         print '\n\nCount :',count
         print 'input:',input,' Dec:',inputDec
@@ -58,13 +64,37 @@ def run():
         hidden_layer_1 = sigmoid(np.dot(input,weight_matrix_1)+bais_1)
         # hidden_layer_2 = sigmoid(np.dot(hidden_layer_1,weight_matrix_2) + bais_2)
         output = sigmoid(np.dot(hidden_layer_1,weight_matrix_output)+bais_3)
-        steped = step(output)
+        staped = step(output)
         error = np.square(target - output)*1/2
-        input = steped
+
+        input = input.reshape(1,4)
+
+        # gradidant
+        delta_out = np.multiply((output - target), sigmoidPrime(np.dot(hidden_layer_1,weight_matrix_output)+bais_3))
+        dedw2 = np.dot(hidden_layer_1.T , delta_out)
+        delta1 = np.dot(delta_out, weight_matrix_output.T) * sigmoidPrime(np.dot(input,weight_matrix_1)+bais_1)
+        dedw1 = np.dot(input.T, delta1)
+        # correction
+        weight_matrix_1 = weight_matrix_1 - dedw1
+        weight_matrix_output = weight_matrix_output - dedw2
+
+        input = staped.reshape(-1)
         count += 1
-        print 'output:',steped,' Dec:',b2d(np.array2string(steped,separator='')[2:-2])
+
+        print 'output:',staped,' Dec:',b2d(np.array2string(staped,separator='')[2:-2])
         print 'target :',target,' Dec:',b2d(np.array2string(target,separator='')[1:-1])
         print 'error :',error
         print 'Toral error:',error.sum()
+
+        # print "weight_matrix_1 gradidant :",dedw1
+        # print "weight_matrix_1 orign :",weight_matrix_1.shape
+        # print "weight_matrix_output gradidant :",dedw2
+        # print "weight_matrix_output orign :",weight_matrix_output.shape
+
+    # learned data
+    print 'weight_matrix_1 :',weight_matrix_1
+    print '\n weight_matrix_output :',weight_matrix_output
+
+
 
 run()
